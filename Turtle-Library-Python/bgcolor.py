5e653650731930e4236e7f20e4d3b4f2b19deb26
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2022-10-15 14:04:52 +0530

    Create bgcolor.py

---
+++ Turtle-Library-Python/bgcolor.py
@@ -0,0 +1,12 @@
+import turtle 
+t = turtle.Turtle()
+t.color("red")
+t.shape("turtle")
+wn = turtle.Screen()
+wn.bgcolor("black")
+
+# to get the colormode of the screen type the following command in the shell
+# >>> turtle.colormode()
+
+turtle.colormode(255) # this colormode is set for the RGB mode
+wn.bgcolor(170,30,20)
