3821f293a12668be31491abdf62a17be659bab3c
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2022-10-15 13:28:16 +0530

    Update Shape.py

--- Turtle-Library-Python/Shape.py
+++ Turtle-Library-Python/Shape.py
@@ -2,3 +2,8 @@ import turtle
 t= turtle.Turtle()
 t.shape() # to get the default shape of the cursor which is 'classic'
 t.shape("turtle")# to change the shape of the cursor
+
+# To get the full informatio about the full information of the shape option in turtle library
+Type the following command in the shell
+>> import turtle
+>> help(turtle.shape)
