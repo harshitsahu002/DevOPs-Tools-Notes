483bb8283c27546fc1c2199a8a95b5cfb8bcceff
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2022-10-15 14:08:29 +0530

    Create bgpic.py

---
+++ Turtle-Library-Python/bgpic.py
@@ -0,0 +1,8 @@
+# this tool is ued to set the background with a picture but with two necessary conditions
+#  1. the image should be in the .gif format
+#  2. the code file and image file should be present in the folder
+
+import turtle
+t = turtle.Turtle()
+wn = turtle.Screen()
+wn.bgpic("pythonimg.gif")
