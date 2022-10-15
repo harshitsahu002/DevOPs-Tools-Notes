61c4bebb83ad5e2cbf8d15c8166553bb74695c0e
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2022-10-15 13:51:06 +0530

    Create forward-backward.py

---
+++ Turtle-Library-Python/forward-backward.py
@@ -0,0 +1,10 @@
+import turtle
+t= turtle.Turtle()
+t.shape("turtle")
+t.forward(100) # turtle move 100 pixels forward
+t.fd(100) # shortcut of the above command
+t.forward(-100) # turtle will move 100 pixels backward
+
+t.backward(100) # turtle move 100 pixels backward
+t.bk(100) # shortcut of the above command
+t.backward(-200) # turtle move 200 pixels forward
