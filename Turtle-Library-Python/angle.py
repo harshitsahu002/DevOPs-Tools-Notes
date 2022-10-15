5d6bae5f1e11efa84e964cfed4a02ac889af0e85
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2022-10-15 14:22:58 +0530

    Create angle.py

---
+++ Turtle-Library-Python/angle.py
@@ -0,0 +1,8 @@
+import turtle 
+t = turtle.Turtle()
+t.forward(100)
+t.left(45) # through this the cursor move 45 degree to its left
+t.fd(100)
+
+t.right(75) # through this the cursor move 75 degree to its right
+t.forward(100)
