25b3aca6a99bb29f0a0e2ebdad90539dfbbc3424
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2022-10-15 14:27:36 +0530

    Create square.py

---
+++ Turtle-Library-Python/square.py
@@ -0,0 +1,20 @@
+import turtle
+t = turtle.Turtle()
+t.shape("circle")
+t.color("red")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+# OR 
+
+for i in range(4):
+  t.forward(100)
+  t.left(90)
+  
+
