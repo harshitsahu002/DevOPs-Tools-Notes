c0f865b310bec138333f72baaa73fea495cecda4
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2022-10-15 13:43:32 +0530

    Create Color.py

---
+++ Turtle-Library-Python/Color.py
@@ -0,0 +1,6 @@
+import turtle
+t = turtle.Turtle() # here we are making an object of the turtle 
+t.color() # by default it is ('black','black') where first 'black' is color of the line and the second 'black' is color of the turtle
+t.color("red") # both color will be red
+t.color("blue","green") # color of line = blue ; color of the turtle = green
+
