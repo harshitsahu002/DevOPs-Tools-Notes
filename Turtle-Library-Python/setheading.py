35d5c213a2dc58b22471c5c5ae15021d1e9173ea
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2022-10-15 14:14:01 +0530

    Create setheading.py

---
+++ Turtle-Library-Python/setheading.py
@@ -0,0 +1,7 @@
+import turtle
+t = turtle.Turtle()
+t.setheading(90) # through this our cursor move 90 degree to its left 
+t.setheading(180) # through this our cursor move 180 degree to its left form its intial position
+t.setheading(270) 
+t.setheading(360) # back to intial position
+t.setheading(100) 
