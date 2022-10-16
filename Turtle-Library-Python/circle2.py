294784a932a6cb9b61e68c27020ae18f460e4202
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2022-10-16 22:00:26 +0530

    Create circle2.py

---
+++ Turtle-Library-Python/circle2.py
@@ -0,0 +1,18 @@
+# PROGRAM 1
+
+import turtle
+t = turtle.Turtle()
+t.color("white")
+wn = turtle.Screen()
+wn.bgcolor("black")
+t.begin_fill()
+t.fillcolor("yellow")
+t.circle(100)
+t.end_fill()
+t.hideturtle()
+
+# PROGRAM 2
+t.circle(100,steps = 3)
+
+#PROGRAM 3
+t.circle(100,steps = 8)
